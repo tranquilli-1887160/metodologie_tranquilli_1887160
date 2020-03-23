e3cdac553d9f582eed16a7fd8b9a58cf121507c2
tranquilli-1887160 <62557644+tranquilli-1887160@users.noreply.github.com>
2020-03-23 16:23:18 +0100

    Update TestLettore.java

--- TestLettore.java
+++ TestLettore.java
@@ -3,9 +3,8 @@ package lab3;
 public class TestLettore {
 
 	public static void main(String[] args) {
-		LettoreTesto x=new LettoreTesto();
 		String text="ciao mio caro amico di infanzia e amico di scuola";
-		x.contaParola(text,"amico");
+		contaParola(text,"amico");
 
 	}
 
